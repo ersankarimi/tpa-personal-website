7370190b0c14899cf98983a41191311094f9bc4f
ersankarimi <ersankarimii311@gmail.com>
2022-10-10 21:55:38 +0800

    Refactor event for navbar
    Move all logic to handle navbar into navbar.js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,3 @@
-// set toggle display navbar
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const closeMenu = document.querySelector(".close-menu");
-
-hamburgerMenu.addEventListener("click", () => {
-  const navList = document.querySelector(".nav-list");
-  navList.style.top = 0;
-  document.body.style.overflow = "hidden";
-});
-
-closeMenu.addEventListener("click", () => {
-  const navList = document.querySelector(".nav-list");
-
-  navList.style.top = "-1000px";
-  document.body.style.overflow = "auto";
-});
-
 document.body.addEventListener("mousemove", (event) => {
   const sectionRight = document.querySelector("section#right");
   const accessories1 = document.querySelector(".accessories-1");

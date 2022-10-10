68b199f4b35c93486670ee2c9f57db5f1b2db68d
ersankarimi <ersankarimii311@gmail.com>
2022-10-10 13:18:35 +0800

    Create index.js
    Set toggle display for navbar

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// set toggle display navbar
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  const navList = document.querySelector(".nav-list");
+  navList.style.top = 0;
+});
+
+closeMenu.addEventListener("click", () => {
+  const navList = document.querySelector(".nav-list");
+  navList.style.top = "-1000px";
+});
